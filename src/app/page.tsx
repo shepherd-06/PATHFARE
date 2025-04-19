1ac5b54c9c35eae08fb1df394ff13d243f41af36
Mohammad Asif Ibtehaz <ibtehaz.shawon@gmail.com>
2025-04-19 15:12:35 +0000

    Center Align Pathfare.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 
 export default function Home() {
   return (
-    <div className="container mx-auto p-4 max-w-3xl">
+    <div className="container mx-auto p-4 max-w-3xl text-center">
       <h1 className="text-3xl font-bold mb-4 text-primary">Pathfare</h1>
       <h3 className="opacity-70">
         Unlock seamless shipping with Pathfare. Optimize your routes and packaging for cost-effective and timely
@@ -13,4 +13,3 @@ export default function Home() {
     </div>
   );
 }
-
